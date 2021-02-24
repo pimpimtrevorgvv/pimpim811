8c746250bb1881526a094393f3ad6f730303fc46
pimpimtrevorgvv <79608169+pimpimtrevorgvv@users.noreply.github.com>
2021-02-24 18:48:15 -0400

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,125 @@
+const help = (prefix) => {
+	return `
+「 *〘𝐑𝐄𝐁〙 〘𝐁𝐎𝐓〙* 」
+
+◪ *informações*
+  ❏ Prefix: 「  ${prefix}  」
+  ❏ Criador : 『𝐏𝐢𝐦𝐏𝐢𝐦』
+  ❏ https://wa.me/994407273548
+
+◪ *SOBRE*
+  │
+  ├─ ❏ ${prefix}info
+  ├─ ❏ ${prefix}blocklist
+  ├─ ❏ ${prefix}chatlist
+  ├─ ❏ ${prefix}ping
+  └─ ❏ ${prefix}bugreport
+◪ *FAZER*
+  │
+  ├─ ❏ ${prefix}sticker
+  ├─ ❏ ${prefix}stickergif
+  ├─ ❏ ${prefix}toimg
+  ├─ ❏ ${prefix}tomp3
+  ├─ ❏ ${prefix}bpink
+  ├─ ❏ ${prefix}marvellogo
+  ├─ ❏ ${prefix}snowwrite
+  ├─ ❏ ${prefix}3dtext
+  ├─ ❏ ${prefix}ninjalogo
+  ├─ ❏ ${prefix}water
+  ├─ ❏ ${prefix}firetext
+  ├─ ❏ ${prefix}logowolf
+  ├─ ❏ ${prefix}logowolf2
+  ├─ ❏ ${prefix}phlogo
+  ├─ ❏ ${prefix}glitch
+  ├─ ❏ ${prefix}neonlogo
+  ├─ ❏ ${prefix}neonlogo2
+  ├─ ❏ ${prefix}lionlogo
+  ├─ ❏ ${prefix}jokerlogo
+  ├─ ❏ ${prefix}shadow
+  ├─ ❏ ${prefix}burnpaper
+  ├─ ❏ ${prefix}coffee
+  ├─ ❏ ${prefix}lovepaper
+  ├─ ❏ ${prefix}woodblock
+  ├─ ❏ ${prefix}qowheart
+  ├─ ❏ ${prefix}mutgrass
+  ├─ ❏ ${prefix}undergocean
+  ├─ ❏ ${prefix}woodenboards
+  ├─ ❏ ${prefix}wolfmetal
+  ├─ ❏ ${prefix}metalictglow
+  ├─ ❏ ${prefix}8bit
+  ├─ ❏ ${prefix}ttp
+  ├─ ❏ ${prefix}herrypotter
+  ├─ ❏ ${prefix}pubglogo
+  └─ ❏ ${prefix}quotemaker
+◪ *MEDIA*
+  │
+  ├─ ❏ ${prefix}trendtwit
+  ├─ ❏ ${prefix}randomkpop
+  └─ ❏ ${prefix}ytsearch
+◪ *EDUCATION*
+  │
+  ├─ ❏ ${prefix}wiki
+  ├─ ❏ ${prefix}wikien
+  ├─ ❏ ${prefix}nulis
+  ├─ ❏ ${prefix}quotes
+  ├─ ❏ ${prefix}quotes2
+  └─ ❏ ${prefix}artinama
+◪ *KERANG AJAIB*
+  │
+  ├─ ❏ ${prefix}apakah
+  ├─ ❏ ${prefix}kapankah
+  ├─ ❏ ${prefix}rate
+  └─ ❏ ${prefix}bisakah
+◪ *DOWNLOADER*
+  │
+  ├─ ❏ ${prefix}images
+  ├─ ❏ ${prefix}ytmp3
+  ├─ ❏ ${prefix}ytmp4
+  ├─ ❏ ${prefix}tiktok
+  └─ ❏ ${prefix}joox
+◪ *MEME*
+  │
+  ├─ ❏ ${prefix}meme
+  └─ ❏ ${prefix}memeindo
+◪ *SOM*
+  │
+  ├─ ❏ ${prefix}play
+  └─ ❏ ${prefix}tts
+◪ *MÚSICA*
+  │
+  ├─ ❏ ${prefix}lirik
+  └─ ❏ ${prefix}chord
+◪ *ISLAM*
+  │
+  └─ ❏ ${prefix}quran
+◪ *STALK*
+  │
+  ├─ ❏ ${prefix}tiktokstalk
+  └─ ❏ ${prefix}igstalk
+◪ *WIBU*
+  │
+  ├─ ❏ ${prefix}neonime
+  ├─ ❏ ${prefix}pokemon
+  ├─ ❏ ${prefix}loli
+  ├─ ❏ ${prefix}waifu
+  ├─ ❏ ${prefix}randomanime
+  ├─ ❏ ${prefix}husbu
+  ├─ ❏ ${prefix}husbu2
+  ├─ ❏ ${prefix}wait
+  └─ ❏ ${prefix}nekonime
+◪ *DIVERSÃO*
+  │
+  ├─ ❏ ${prefix}alay
+  ├─ ❏ ${prefix}gantengcek
+  ├─ ❏ ${prefix}watak
+  ├─ ❏ ${prefix}hobby
+  ├─ ❏ ${prefix}game
+  ├─ ❏ ${prefix}bucin
+  ├─ ❏ ${prefix}trust
+  ├─ ❏ ${prefix}dare
+  └─ ❏ ${prefix}simi
+
+`
+}
+
+exports.help = help
---
+++ src/toinmenu.js
@@ -0,0 +1,12 @@
+const toinmenu = (prefix, pushname) => {
+    return `◪ *Comandos do PimPim*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.toinmenu = toinmenu
